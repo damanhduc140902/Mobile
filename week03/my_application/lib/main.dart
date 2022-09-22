3a3c1a099080a19f17e7e885a29138df2142c128
damanhduc140902 <80797626+damanhduc140902@users.noreply.github.com>
2022-09-22 08:27:05 -0700

    Button add an activity

--- week03/my_application/lib/main.dart
+++ week03/my_application/lib/main.dart
@@ -9,8 +9,17 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: _title, home: MyTodoList());
+    return MaterialApp(
+        title: _title,
+        home: MyTodoList(
+        ));
   }
+}
+
+class Task {
+  Task({required this.description});
+
+  String description;
 }
 
 class MyTodoList extends StatefulWidget {
@@ -21,15 +30,91 @@ class MyTodoList extends StatefulWidget {
 }
 
 class _MyTodoListState extends State<MyTodoList> {
+  final _formKey = GlobalKey<FormState>();
+
+  Future<void> addActivity() {
+    return showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text('Describe your activity'),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Form(
+                    key: _formKey,
+                    child: TextFormField(
+                        maxLines: null)),
+              ],
+            ),
+            actions: <Widget>[
+              TextButton(
+                child: const Text('Cancel'),
+                onPressed: null,
+              ),
+              TextButton(
+                onPressed: null,
+                child: const Text('Add'),
+              )
+            ],
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(centerTitle: true, title: const Text('My To-do List')),
       body: null,
       floatingActionButton: FloatingActionButton(
-          onPressed: null,
+          onPressed: () => addActivity(),
           tooltip: 'Add an activity',
           child: const Icon(Icons.add)),
     );
+  }
+}
+class MyActivity extends StatefulWidget {
+  MyActivity(
+      {required this.task, required this.finished, required this.deleted})
+      : super(key: ObjectKey(task));
+
+  bool finished;
+  Task task;
+  bool deleted;
+
+  @override
+  State<MyActivity> createState() => _MyActivityState();
+}
+class _MyActivityState extends State<MyActivity> {
+  final _formKey = GlobalKey<FormState>();
+  final myController = TextEditingController();
+
+  @override
+  void dispose() {
+    myController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+        onTap: null,
+        leading: Checkbox(
+            checkColor: Colors.white,
+            activeColor: Colors.blue,
+            value: widget.finished,
+            onChanged: (bool? value) {
+              setState(() {
+                widget.finished = value!;
+              });
+            }),
+        title: Text(widget.task.description),
+        trailing: PopupMenuButton(
+            itemBuilder: ((context) => [
+              const PopupMenuItem(
+                  value: null , child: Text('Edit')),
+              const PopupMenuItem(
+                  value: null, child: Text('Delete'))
+            ])));
   }
 }
