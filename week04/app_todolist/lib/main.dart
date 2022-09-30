3bd9ba67c2296ceb6ab04f4a7bf92282ad806424
Hotaru852 <91526646+Hotaru852@users.noreply.github.com>
2022-09-30 12:12:46 +0700

    ManageCategories display

--- week04/app_todolist/lib/main.dart
+++ week04/app_todolist/lib/main.dart
@@ -1,190 +1,338 @@
-import 'package:app_todolist/AppAssets.dart';
-import 'package:app_todolist/AppColors.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
-import 'package:app_todolist/MyHomePage.dart';
+enum Options { edit, hide, delete }
 
-void main() {
-  runApp(const MyApp());
-}
+const Color boldBlue = Color.fromARGB(255, 125, 171, 245);
+const Color lightBlue = Color.fromARGB(255, 224, 238, 253);
+const Color boldBoldBlue = Color.fromARGB(255, 46, 138, 237);
 
-class MyApp extends StatefulWidget {
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
+  static const String _title = 'To-do List';
 
-class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-        home: Scaffold(
-      body: MyHomePage(),
-    ));
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      // width: 428,
-      // height: 926,
-      child: Column(
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Container(
-                  margin: EdgeInsets.all(10),
-                  decoration: BoxDecoration(color: AppColors.primaryColor),
-                  child: TextButton(
-                      onPressed: () {},
-                      child: Column(
-                        children: [
-                          Text(
-                            "Tất cả",
-                            style: GoogleFonts.roboto(
-                                textStyle: TextStyle(
-                                    fontSize: 13, color: Colors.black)),
-                          ),
-                        ],
-                      ))),
-              Container(
-                  margin: EdgeInsets.all(10),
-                  decoration: BoxDecoration(color: AppColors.primaryColor),
-                  child: TextButton(
-                      onPressed: () {},
-                      child: Column(
-                        children: [
-                          Text(
-                            "Công việc",
-                            style: GoogleFonts.roboto(
-                                textStyle: TextStyle(
-                                    fontSize: 13, color: Colors.black)),
-                          ),
-                        ],
-                      ))),
-              Container(
-                  margin: EdgeInsets.all(10),
-                  decoration: BoxDecoration(color: AppColors.primaryColor),
-                  child: TextButton(
-                      onPressed: () {},
-                      child: Column(
-                        children: [
-                          Text(
-                            "Cá nhân",
-                            style: GoogleFonts.roboto(
-                                textStyle: TextStyle(
-                                    fontSize: 13, color: Colors.black)),
-                          ),
-                        ],
-                      ))),
-              Container(
-                  margin: EdgeInsets.all(10),
-                  decoration: BoxDecoration(color: AppColors.primaryColor),
-                  child: TextButton(
-                      onPressed: () {},
-                      child: Column(
-                        children: [
-                          Text(
-                            "Danh sách yêu thích",
-                            style: GoogleFonts.roboto(
-                                textStyle: TextStyle(
-                                    fontSize: 13, color: Colors.black)),
-                          ),
-                        ],
-                      ))),
-              Container(
-                  margin: EdgeInsets.all(10),
-                  decoration: BoxDecoration(color: AppColors.primaryColor),
-                  child: TextButton(
-                      onPressed: () {},
-                      child: Column(
-                        children: [
-                          Text(
-                            "Ngày sinh nhật",
-                            style: GoogleFonts.roboto(
-                                textStyle: TextStyle(
-                                    fontSize: 13, color: Colors.black)),
-                          ),
-                        ],
-                      ))),
-              Container(
-                margin: EdgeInsets.all(10),
-                child: TextButton(
-                  onPressed: () {},
-                  child: Image.asset(AppAssets.add),
-                ),
-              )
-            ],
-          ),
-          
-          Spacer(),
-          Container(
-              width: 100,
-              height: 50,
-              child: TextButton(
-                  onPressed: () {}, child: Image.asset(AppAssets.addtask))),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Container(
-                padding: const EdgeInsets.all(15),
-                child: Image.asset(AppAssets.menu),
-              ),
-              Container(
-                margin: const EdgeInsets.all(10),
-                child: TextButton(
-                    onPressed: () {},
-                    child: Column(
-                      children: [
-                        Image.asset(AppAssets.nhiemvu),
-                        Text("Nhiệm vụ",
-                            style: GoogleFonts.roboto(
-                                fontSize: 10, color: Colors.black))
-                      ],
-                    )),
-              ),
-              Container(
-                // margin: const EdgeInsets.all(10),
-                child: TextButton(
-                    onPressed: () {},
-                    child: Column(
-                      children: [
-                        Image.asset(AppAssets.lich),
-                        Text("Lịch",
-                            style: GoogleFonts.roboto(
-                                fontSize: 10, color: Colors.black))
-                      ],
-                    )),
-              ),
-              Container(
-                // margin: const EdgeInsets.all(10),
-                child: TextButton(
-                    onPressed: () {},
-                    child: Column(
-                      children: [
-                        Image.asset(AppAssets.cuatoi),
-                        Text("Của tôi",
-                            style: GoogleFonts.roboto(
-                                fontSize: 10, color: Colors.black))
-                      ],
-                    )),
-              )
-            ],
-          )
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: _title,
+      home: ManageCategories(
+        categories: [
+          Category(description: 'Work', counter: 0),
+          Category(description: 'Personal', counter: 0),
+          Category(description: 'Wishlist', counter: 0),
+          Category(description: 'Birthday', counter: 0)
         ],
       ),
+      theme: ThemeData(
+          //Brightness and colors
+          brightness: Brightness.light,
+
+          //Font family
+          fontFamily: 'Roboto',
+
+          //Scaffold theme
+          scaffoldBackgroundColor: const Color.fromARGB(255, 249, 255, 255),
+
+          //Appbar theme
+          appBarTheme: const AppBarTheme(
+              elevation: 0,
+              backgroundColor: Color.fromARGB(255, 249, 255, 255),
+              iconTheme: IconThemeData(color: Colors.black, size: 30),
+              titleTextStyle: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black))),
+
+      //Text themes
+      // textTheme: const TextTheme(
+
+      //     ),
     );
   }
+}
+
+class ManageCategories extends StatefulWidget {
+  ManageCategories({super.key, required this.categories});
+
+  List<Category> categories;
+
+  @override
+  State<ManageCategories> createState() => ManageCategoriesState();
+}
+
+class ManageCategoriesState extends State<ManageCategories> {
+  final formKey = GlobalKey<FormState>();
+  final controller = TextEditingController();
+
+  void newCategory() {
+    setState(() {
+      if ((widget.categories
+              .where((element) => element.description == controller.text))
+          .isEmpty) {
+        widget.categories
+            .add(Category(description: controller.text, counter: 0));
+        Navigator.pop(context);
+        controller.clear();
+      }
+    });
+  }
+
+  Future<void> addCategory() {
+    return showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text('Create new category'),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Form(
+                    child: TextFormField(
+                  decoration: const InputDecoration(hintText: 'Input here'),
+                  key: formKey,
+                  keyboardType: TextInputType.multiline,
+                  minLines: 3,
+                  maxLines: 5,
+                  maxLength: 50,
+                  controller: controller,
+                ))
+              ],
+            ),
+            actions: <Widget>[
+              TextButton(
+                onPressed: () => {Navigator.pop(context), controller.clear()},
+                child: const Text('CANCEL'),
+              ),
+              TextButton(
+                  onPressed: () => newCategory(), child: const Text('SAVE'))
+            ],
+          );
+        });
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          leading: const Icon(Icons.arrow_back),
+          title: const Padding(
+              padding: EdgeInsets.only(left: 0),
+              child: Text('Manage Categories')),
+        ),
+        body: Column(
+          children: [
+            Container(
+                color: lightBlue,
+                child: ListTile(
+                  title: Center(
+                    child: Text(
+                      'Categories display on homepage',
+                      style: TextStyle(color: Colors.black.withOpacity(0.6)),
+                    ),
+                  ),
+                )),
+            ReorderableListView(
+                shrinkWrap: true,
+                buildDefaultDragHandles: true,
+                onReorder: ((oldIndex, newIndex) {
+                  setState(() {
+                    if (oldIndex < newIndex) {
+                      newIndex -= 1;
+                    }
+                    final Category item = widget.categories.removeAt(oldIndex);
+                    widget.categories.insert(newIndex, item);
+                  });
+                }),
+                children: widget.categories.map((category) {
+                  return Categories(
+                      category: category);
+                }).toList()),
+            GestureDetector(
+                onTap: () => addCategory(),
+                child: Row(
+                  children: const <Widget>[
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(16, 20, 0, 20),
+                      child: Icon(Icons.add, color: boldBoldBlue, size: 25),
+                    ),
+                    Expanded(
+                        child: Padding(
+                            padding: EdgeInsets.only(left: 15),
+                            child: Text('Create New',
+                                style: TextStyle(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold,
+                                    color: boldBoldBlue))))
+                  ],
+                ))
+          ],
+        ));
+  }
+}
+
+class Categories extends StatefulWidget {
+  Categories(
+      {required this.category})
+      : super(key: ObjectKey(category));
+
+  Category category;
+
+  @override
+  State<Categories> createState() => CategoriesState();
+}
+
+class CategoriesState extends State<Categories> {
+  final formKey = GlobalKey<FormState>();
+  final controller = TextEditingController();
+  bool deleted = false;
+  bool visible = true;
+  String text = 'Hide';
+
+  void newCategory() {
+    setState(() {
+      widget.category.description = controller.text;
+      Navigator.pop(context);
+      controller.clear();
+    });
+  }
+
+  Future<void> editCategory() {
+    return showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text('Create new category'),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Form(
+                    child: TextFormField(
+                  decoration: const InputDecoration(
+                    hintText: 'Input here.',
+                    // border: OutlineInputBorder(
+                    //     borderSide: BorderSide(
+                    //         width: 0, style: BorderStyle.none)),
+                  ),
+                  keyboardType: TextInputType.multiline,
+                  maxLines: null,
+                  minLines: 3,
+                  maxLength: 50,
+                  controller: controller,
+                ))
+              ],
+            ),
+            actions: <Widget>[
+              TextButton(
+                  onPressed: () => {Navigator.pop(context), controller.clear()},
+                  child: const Text('CANCEL')),
+              TextButton(
+                  onPressed: () => newCategory(), child: const Text('SAVE'))
+            ],
+          );
+        });
+  }
+
+  Future<void> deleteCategory() {
+    return showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text('Delete the category?'),
+            content: const Text('All tasks in this category will be deleted'),
+            actions: <Widget>[
+              TextButton(
+                  onPressed: () => {Navigator.pop(context)},
+                  child: const Text('CANCEL')),
+              TextButton(
+                  onPressed: () {
+                    setState(() {
+                      deleted = true;
+                      Navigator.pop(context);
+                    });
+                  },
+                  child: const Text('DELETE'))
+            ],
+          );
+        });
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (!deleted) {
+      return Row(
+        children: <Widget>[
+          Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Container(
+                  width: 15,
+                  height: 15,
+                  decoration: const BoxDecoration(
+                      shape: BoxShape.circle, color: boldBlue))),
+          Expanded(
+              child: Text(widget.category.description,
+                  style: const TextStyle(
+                      fontWeight: FontWeight.bold, fontSize: 20))),
+          if (!visible)
+            const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                child: Icon(Icons.visibility_off)),
+          Padding(
+              padding: const EdgeInsets.fromLTRB(20, 20, 20, 20),
+              child: Text(widget.category.counter.toString())),
+          PopupMenuButton<Options>(
+              tooltip: '',
+              onSelected: (Options item) {
+                if (item == Options.delete) {
+                  deleteCategory();
+                } else if (item == Options.hide) {
+                  setState(() {
+                    visible = !visible;
+                    text = !visible ? 'Show' : 'Hide';
+                  });
+                } else {
+                  editCategory();
+                }
+              },
+              itemBuilder: ((context) => [
+                    const PopupMenuItem<Options>(
+                      value: Options.edit,
+                      child: Text('Edit'),
+                    ),
+                    PopupMenuItem<Options>(
+                      value: Options.hide,
+                      child: Text(text),
+                    ),
+                    const PopupMenuItem<Options>(
+                      value: Options.delete,
+                      child: Text('Delete'),
+                    )
+                  ]))
+        ],
+      );
+    } else {
+      return const SizedBox.shrink();
+    }
+  }
+}
+
+class Category {
+  Category({required this.description, required this.counter});
+
+  String description;
+  int counter;
 }
