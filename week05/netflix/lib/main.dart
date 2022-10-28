94423acf8c0585be178b94c247662a2b5e2e81f3
damanhduc140902 <80797626+damanhduc140902@users.noreply.github.com>
2022-10-27 20:24:05 -0700

    Add files via upload

---
+++ week05/netflix/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:netflix_clone/feature/loginscreen/loginscreen.dart';
+import 'core/init/theme/app_theme.dart';
+import 'feature/choose_account_page/view/choose_account.dart';
+import 'feature/splash_screen/view/splash_screen.dart';
+import 'feature/tabbar/view/app_tabbar_view.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: AppTheme.instance.theme,
+      debugShowCheckedModeBanner: false,
+      home: WelcomeScreen(),
+
+    );
+  }
+}
