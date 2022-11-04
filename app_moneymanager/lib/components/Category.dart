bfcc44bb254a04effef0b63dc0a6aeca11713318
damanhduc140902 <damanhduc140902@gmail.com>
2022-11-04 12:39:12 -0700

    cap nhat giao dien cua app

---
+++ app_moneymanager/lib/components/Category.dart
@@ -0,0 +1,14 @@
+class Category {
+  Category(
+      {this.index = 0,
+      required this.id,
+      required this.icon,
+      required this.color,
+      required this.description});
+
+  String id;
+  int index;
+  String icon;
+  String color;
+  String description;
+}
---
+++ app_moneymanager/lib/globals.dart
@@ -0,0 +1,5 @@
+library cvconnect.globals;
+
+import 'components/ScheduleIndex.dart';
+
+List<IndexSchedule>? listAccount;
