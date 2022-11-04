7752e4c6078cdeeeec0ada71cffe3f1c8c5c0620
damanhduc140902 <80797626+damanhduc140902@users.noreply.github.com>
2022-11-03 22:19:34 -0700

    Make a change

--- week04/app_todolist/lib/AppAssets.dart
+++
@@ -1,9 +0,0 @@
-class AppAssets {
-  static final String imagePath = 'assets//images//';
-  static final String add = imagePath + 'add.png';
-  static final String addtask = imagePath + 'addtask.png';
-  static final String cuatoi = imagePath + 'cuatoi.png';
-  static final String lich = imagePath + 'lich.png';
-  static final String nhiemvu = imagePath + 'nhiemvu.png';
-  static final String menu = imagePath + 'menu.png';
-}
--- week04/app_todolist/lib/AppColors.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter/material.dart';
-
-class AppColors {
-  static const Color primaryColor = Color.fromARGB(255, 81, 126, 230);
-}
